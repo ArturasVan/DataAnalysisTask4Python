05983ddb27390b7b0d3c4cac91f95610a339d07d
ArturasVan <71314526+ArturasVan@users.noreply.github.com>
2021-06-29 00:35:37 +0200

    finished solution

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -3,17 +3,32 @@ import pandas as pd
 import seaborn as sns
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
+from matplotlib.dates import  DateFormatter
+
+months = ['January','February','March','April','May','June','July','August','September','October','November','December']
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
+df = pd.read_csv('fcc-forum-pageviews.csv', parse_dates=['date'],index_col=['date'])
 
 # Clean data
-df = None
+
+df = df[(df['value'] > df['value'].quantile(0.025)) &
+        (df['value'] < df['value'].quantile(0.975))]
 
 
 def draw_line_plot():
     # Draw line plot
 
+    fig, ax = plt.subplots()
+    fig.set_figheight(6)
+    fig.set_figwidth(14)
+
+    ax.plot_date(df.index, df['value'], linestyle="solid", marker=None, color="red")
+    ax.set_ylabel('Page Views')
+    ax.set_xlabel('Date')
+    ax.set_title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
+
+    ax.xaxis.set_major_formatter(DateFormatter('%Y-%m'))
 
 
 
@@ -24,10 +39,24 @@ def draw_line_plot():
 
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
-    df_bar = None
-
+    df_bar = df.copy()
+    df_bar['year'] = df_bar.index.year
+    df_bar['Month'] = df_bar.index.strftime('%B')
+    df_grp = df_bar.groupby(['year', 'Month'])
+    df_grp['value'].apply(lambda x: x.mean())
     # Draw bar plot
+    sns.set_style("ticks")
+    g = sns.catplot(x="year", kind="bar", hue="Month", y="value", data=df_bar, hue_order=months, ci=None, legend=False, palette="hls")
 
+    fig = g.fig
+    ax = g.ax    
+    ax.set_ylabel('Average Page Views')
+    ax.set_xlabel('Years')
+    plt.xticks(rotation=90)
+    plt.legend(loc='upper left', title="Month")
+    plt.setp(ax.get_legend().get_texts(), fontsize='8')
+    plt.setp(ax.get_legend().get_title(), fontsize='8')
+    plt.tight_layout()
 
 
 
@@ -36,6 +65,13 @@ def draw_bar_plot():
     fig.savefig('bar_plot.png')
     return fig
 
+
+# https://github.com/mwaskom/seaborn/issues/915
+# TypeError: box() got an unexpected keyword argument 'label'
+def fixed_boxplot(*args, label=None, **kwargs):
+    sns.boxplot(*args, **kwargs, labels=[label])
+
+
 def draw_box_plot():
     # Prepare data for box plots (this part is done!)
     df_box = df.copy()
@@ -43,8 +79,23 @@ def draw_box_plot():
     df_box['year'] = [d.year for d in df_box.date]
     df_box['month'] = [d.strftime('%b') for d in df_box.date]
 
-    # Draw box plots (using Seaborn)
+    df_box.sort_values(by=['year','date'], ascending=[False, True], inplace=True)
 
+    # Draw box plots (using Seaborn)
+    
+    df_box["Page Views"] = df_box["value"]
+    df_box["Month"] = df_box["month"]
+    df_box["Year"] = df_box["year"]
+    g = sns.PairGrid(df_box, y_vars=["Page Views"], x_vars=["Year", "Month"], palette="hls")
+    g.map(fixed_boxplot)
+    fig = g.fig
+    fig.set_figheight(6)
+    fig.set_figwidth(16)
+    fig.axes[0].set_ylabel('Page Views')
+    fig.axes[1].set_ylabel('Page Views')
+    fig.axes[0].set_title('Year-wise Box Plot (Trend)')
+    fig.axes[1].set_title('Month-wise Box Plot (Seasonality)')
+    plt.tight_layout()
 
 
 
